2cff301a2c3c750ac867982a937c2ee9cae57cf6
Shahanees1998 <shahanees1998@gmail.com>
2024-10-07 12:01:30 +0500

    fix: res fixes

--- components/Statistics/calenderEdit.tsx
+++ components/Statistics/calenderEdit.tsx
@@ -20,13 +20,13 @@ const CalenderEdit: React.FC<CalenderEditProps> = ({
                     <div className="container">
                         <div className="-mx-4 flex flex-wrap items-center relative z-10">
                             <div className="w-full px-5 md:w-1/1 lg:w-1/1 mx-auto">
-                                <div className="px-10 py-5  bg-white shadow-lg rounded-xl">
+                                <div className="px-10 py-2  bg-white shadow-lg rounded-xl" style={{ marginTop: '3rem' }}>
                                     <div className="-mx-4 popup-content flex flex-wrap">
                                         <div className="w-full px-5 md:w-1/2 lg:w-1/2">
-                                            <h2 className="font-semibold text-xl text-black text-left py-5">Voici 10 proposition de titres SEO pour le mot-clé  “mot-clé”</h2>
+                                            <h2 className="font-semibold text-xl text-black text-left pt-5">Voici 10 proposition de titres SEO pour le mot-clé  “mot-clé”</h2>
                                         </div>
                                         <div className="w-full px-5 md:w-1/2 lg:w-1/2">
-                                            <h2 className="font-semibold text-xl text-black text-left py-5 flex items-center justify-end sm-flex-none">Relancer <a href="#"><img className="px-3" src="/images/hero/repeat.png" /></a></h2>
+                                            <h2 className="font-semibold text-xl text-black text-left pt-5 flex items-center justify-end sm-flex-none">Relancer <a href="#"><img className="px-3" src="/images/hero/repeat.png" /></a></h2>
                                         </div>
                                         <div className="w-full px-5 md:w-1/1 lg:w-1/1">
                                             <div className="flex items-center mb-8 mt-12">
@@ -90,12 +90,12 @@ const CalenderEdit: React.FC<CalenderEditProps> = ({
                                         </div>
                                     </div>
 
-                                    <div className="-mx-4 flex items-center justify-end">
+                                    <div className="-mx-4 pt-8 flex items-center justify-end">
                                         <div className="px-5 flex justify-center">
-                                            <button type="button" style={{ backgroundColor: '#1B44C8' }} className="bg-dark-blue px-10 py-2 rounded-full text-white text-1xl">valider</button>
+                                            <button type="button" style={{ backgroundColor: '#1B44C8' }} className="bg-dark-blue px-6 h-9 rounded-full text-white text-1xl">valider</button>
                                         </div>
                                         <div className="px-5 flex justify-center">
-                                            <button type="button" style={{ backgroundColor: '#1B44C8' }} className="bg-dark-blue px-10 py-2 rounded-full text-white text-1xl" onClick={onClose}>Cancel</button>
+                                            <button type="button" style={{ backgroundColor: '#1B44C8' }} className="bg-dark-blue px-6 h-9 rounded-full text-white text-1xl" onClick={onClose}>Cancel</button>
                                         </div>
                                     </div>
                                 </div>
--- components/Statistics/createClient.tsx
+++ components/Statistics/createClient.tsx
@@ -12,12 +12,12 @@ const CreateClient: React.FC<CreateClientProps> = ({
 }) => {
   if (!isOpen) return null
   return (
-        <div style={{ paddingInline: '10px', marginTop: '20px' }} className="fixed inset-0 z-[9999999999] flex justify-center items-center bg-black bg-opacity-60 text-defaultDark">
+        <div style={{ paddingInline: '10px' }} className="fixed inset-0 z-[9999999999] flex justify-center items-center bg-black bg-opacity-60 text-defaultDark">
             <div style={{ width: '45rem' }} className="px-10 py-5 bg-white shadow-lg rounded-xxl">
-                <h2 className="font-semibold text-xl text-blue text-center border-b py-5 mb-6">Ajouter un client</h2>
+                <h2 className="font-semibold text-xl text-blue text-center border-b mb-8">Ajouter un client</h2>
                 <div className="-mx-4 flex flex-wrap">
                     <div className="w-full px-5 md:w-1/2 lg:w-1/2">
-                        <div className="mb-10">
+                        <div className="mb-6">
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-left">Entreprise</label>
                             <input type="email" id="email" className="px-5 py-3 bg-light text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                         </div>
@@ -25,7 +25,7 @@ const CreateClient: React.FC<CreateClientProps> = ({
                 </div>
                 <div className="-mx-4 w-full flex flex-wrap">
                     <div className="w-full px-5 md:w-2/4 lg:w-2/4">
-                        <div className="mb-5">
+                        <div className="mb-4">
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-left">Nom </label>
                             <input type="email" id="email" className="px-5 py-3 bg-light text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Saisir le nom" required />
                         </div>
@@ -37,7 +37,7 @@ const CreateClient: React.FC<CreateClientProps> = ({
                         </div>
                     </div>
                     <div className="w-full px-5 md:w-1/1 lg:w-1/1">
-                        <div className="mb-10">
+                        <div className="mb-4">
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-left">Adresse Email  </label>
                             <input type="email" id="email" className="px-5 py-3 bg-light text-gray-900 text-sm rounded-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Saisir l’adresse e-mail" required />
                         </div>
@@ -46,7 +46,7 @@ const CreateClient: React.FC<CreateClientProps> = ({
                         <button style={{ backgroundColor: '#1B44C8' }} type="button" className="bg-blue px-10 py-2 rounded-xl text-white text-xl">Ajouter</button>
                     </div>
                     <div className="w-full px-5 md:w-1/1 lg:w-1/1 flex cursor-pointer justify-center" onClick={onClose}>
-                        <img className="mt-12 w-6 h-6" src="/images/hero/Close.png" />
+                        <img className="mt-4 w-6 h-6" src="/images/hero/Close.png" />
                     </div>
                 </div>
             </div>
